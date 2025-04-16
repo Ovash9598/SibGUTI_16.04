d2524466db3e1931901f13f6a028aec7a5ba6807
Ovash9598 <i.budnikov2021@gmail.com>
2025-04-16 09:10:15 +0700

    Add files via upload

---
+++ Задача2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+class Program
+{
+     static int Main()
+    {
+
+
+        Console.Write("Введите название заклинания: ");
+        string spellName = Console.ReadLine();
+
+        string newSpellName = spellName.ToUpper();
+        int l = spellName.Length;
+
+        if (string.IsNullOrWhiteSpace(spellName))
+        {
+
+            Console.WriteLine("Ошибка: название заклинания не может быть пустым!");
+            return 1;
+        }
+        Console.WriteLine($"Название заклинания: {newSpellName}");
+        Console.WriteLine($"Длина названия: {l}");
+        return 0;
+    }
+}
